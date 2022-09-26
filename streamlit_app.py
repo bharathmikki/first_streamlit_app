c04d8deb320384c7205644e205ef4caf23172508
Bharath <100200668+bharathmikki@users.noreply.github.com>
2022-09-26 13:05:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ def get_fruityvice_data(this_fruit_choice):
     # write your own comment - what does this do?
     return fruityvice_normalized
 
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -51,7 +51,7 @@ def get_fruit_load_list():
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()    
     
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     my_cnx.close()
