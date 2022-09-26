f704a4ad8f86830313dc409684349ce5bb02da12
Bharath <100200668+bharathmikki@users.noreply.github.com>
2022-09-26 12:52:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,20 +45,36 @@ try:
     
 except URLError as e:
   streamlit. error()
-  
-streamlit.stop()
-
-
-
+ 
+def get_fruit_load_list():
+    with my cnx. cursor() as my cur:
+        my cur.execute("select * from fruit load list")
+        return my cur. fetchall()    
+    
+if streamlit.button('Get Fruit Load List'):
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The Fruitload List contains:")
-streamlit.dataframe(my_data_rows)
+my_data_rows = get fruit load list()
+streamlit.dataframe (my data rows)    
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add')
-streamlit.write('The user entered ', add_my_fruit)
+def insert row snowflake(new fruit):
+    with my cnx.cursor() as my cur:
+        my cur.execute("insert into fruit load list values ('from streamlit')")
+        return "Thanks for adding + new fruit"
+
+add my fruit = streamlit.text input ('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+    my cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert row snowflake(add my fruit)
+    streamlit.text (back from function)
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("select * from fruit_load_list")
+# my_data_rows = my_cur.fetchall()
+# streamlit.header("The Fruitload List contains:")
+# streamlit.dataframe(my_data_rows)
+
+# add_my_fruit = streamlit.text_input('What fruit would you like to add')
+# streamlit.write('The user entered ', add_my_fruit)
 
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+# my_cur.execute("insert into fruit_load_list values ('from streamlit')")
